ee7b751e5c06af8102e65eb88713d24f9e582102
Chuck Schultz <theraceblogger@comcast.net>
2022-07-26 17:04:59 -0600

    sync cdc cases deaths

---
+++ update_cdc_cases_deaths.py
@@ -0,0 +1,19 @@
+import wget
+import os
+
+
+def sync_cdc():
+	url = 'https://data.cdc.gov/api/views/9mfq-cb36/rows.csv?accessType=DOWNLOAD'
+	filename = '/Users/chuckschultz/covid/data/cdc_cases_deaths.csv'
+	wget.download(url, filename)
+
+	os.system("psql -U chuckschultz -d covid -c \"TRUNCATE cdc.cdc_cases_deaths\"")
+
+	try:
+		os.system(f"psql -U chuckschultz -d covid -c \"COPY cdc.cdc_cases_deaths FROM '{filename}' HEADER DELIMITER ',' CSV\"")
+	except:
+		print("Unable to import cdc_cases_deaths data")
+
+	os.system(f"rm {filename}")
+
+sync_cdc()
