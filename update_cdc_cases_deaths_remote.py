ed683331121c9872f0516f136fe36b1cae5e5716
Chuck Schultz <theraceblogger@comcast.net>
2022-07-29 17:43:15 -0600

    env variables, test operations

--- update_cdc_cases_deaths_remote.py
+++ update_cdc_cases_deaths_remote.py
@@ -22,3 +22,30 @@ sync_cdc()
 
 # path from remote /usr/bin/python3
 # path from local /usr/local/bin/python3
+
+
+# import psycopg2
+# db_name = os.environ['db_name_covid']
+# db_user = os.environ['db_user_covid']
+# db_host = os.environ['db_host_covid']
+# db_credentials = os.environ['db_creds_covid']
+# tmp_data_path = os.environ['tmp_data_path']
+
+
+# connect to database
+def db_connect():
+	conn_string = f'dbname={db_name} user={db_user} host={db_host} password={db_credentials}'
+
+	try:
+		conn = psycopg2.connect(conn_string)
+		conn.autocommit = True
+	except:
+		print('Unable to connect to the database')
+
+	cur = conn.cursor()
+	return cur
+
+# dwh_cur = db_connect()
+
+# truncate_sql = "TRUNCATE public.cdc_cases_deaths"
+	# dwh_cur.execute(truncate_sql)
