5cc35e13d9aed24f66c14dbefba11d194e9ae8e7
Chuck Schultz <theraceblogger@comcast.net>
2022-08-06 12:18:21 -0600

    add hospital and wastewater data

--- update_cdc_cases_deaths.py
+++ update_cdc_cases_deaths.py
@@ -1,5 +1,6 @@
 import wget
 import os
+import sys
 import psycopg2
 from psycopg2.extras import DictCursor
 
@@ -32,13 +33,91 @@ def sync_cdc():
 	filename = f'{tmp_data_path}/cdc_cases_deaths.csv'
 	wget.download(url, filename)
 
-	os.system("psql -U $db_user_covid -d $db_name_covid -h $db_host_covid -c \"TRUNCATE public.cdc_cases_deaths\"")
+	os.system("psql -U $db_user_covid -d $db_name_covid -h $db_host_covid -c \"TRUNCATE raw_data.cdc_cases_deaths\"")
 
 	try:
-		os.system(f"psql -U $db_user_covid -d $db_name_covid -h $db_host_covid -c \"\\COPY public.cdc_cases_deaths FROM '{filename}' HEADER DELIMITER ',' CSV\"")
+		os.system(f"psql -U $db_user_covid -d $db_name_covid -h $db_host_covid -c \"\\COPY raw_data.cdc_cases_deaths FROM '{filename}' HEADER DELIMITER ',' CSV\"")
 	except:
 		print('Unable to import cdc_cases_deaths data')
 
 	os.system(f"rm {filename}")
 
-sync_cdc()
+# sync_cdc()
+
+
+
+def sync_hhs():
+	url = 'https://healthdata.gov/api/views/g62h-syeh/rows.csv?accessType=DOWNLOAD'
+	filename = f'{tmp_data_path}/national_daily_hospitalizations.csv'
+	wget.download(url, filename)
+
+	os.system("psql -U $db_user_covid -d $db_name_covid -h $db_host_covid -c \"TRUNCATE raw_data.national_daily_hospitalizations\"")
+
+	try:
+		os.system(f"psql -U $db_user_covid -d $db_name_covid -h $db_host_covid -c \"\\COPY raw_data.national_daily_hospitalizations FROM '{filename}' HEADER DELIMITER ',' CSV\"")
+	except:
+		print ('Unable to import national_daily_hospitalizations data')
+
+	os.system(f"rm {filename}")
+
+# sync_hhs()
+
+
+def sync_cdc_wastewater():
+	url = 'https://data.cdc.gov/resource/2ew6-ywp6.csv?$limit=500000'
+	filename = f'{tmp_data_path}/cdc_wastewater.csv'
+	wget.download(url, filename)
+
+	os.system("psql -U $db_user_covid -d $db_name_covid -h $db_host_covid -c \"TRUNCATE raw_data.cdc_wastewater\"")
+
+	try:
+		os.system(f"psql -U $db_user_covid -d $db_name_covid -h $db_host_covid -c \"\\COPY raw_data.cdc_wastewater FROM '{filename}' HEADER DELIMITER ',' CSV\"")
+	except:
+		print ('Unable to import cdc_wastewater data')
+
+	os.system(f"rm {filename}")
+
+
+# sync_cdc_wastewater()
+
+
+
+
+# setup initial functions that will redirect CLI to approriate function with zero to 2 params
+def no_params():
+	getattr(sys.modules[__name__], function_name)()
+
+def one_param():
+	getattr(sys.modules[__name__], function_name)(param1)
+
+def two_params():
+	getattr(sys.modules[__name__], function_name)(param1, param2)
+
+if __name__ == "__main__":
+	#first param should be the function
+	function_name = sys.argv[1]
+
+	#second param optional and represents the first parameter for the CLI function
+	try:
+		param1 = sys.argv[2]
+	except Exception:
+		pass
+
+	#third param optional and represents the second parameter for the CLI function
+	try:
+		param2 = sys.argv[3]
+	except Exception:
+		pass
+
+	#determine the number of parameters passed into python.  0 is the file, 1 is the method and params start @ 2
+	param_length = len(sys.argv) - 2
+
+	# map the inputs to the function blocks
+	options = {
+		0 : no_params,
+		1 : one_param,
+		2 : two_params
+		}
+
+	#now call the approriate function which will relay the CLI params to the underlying function
+	options[param_length]()
