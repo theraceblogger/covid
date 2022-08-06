3eadd78c52a34378f785cb1f137e87a4214aba57
Chuck Schultz <theraceblogger@comcast.net>
2022-08-06 16:51:39 -0600

    format

--- sync_update_data.sh
+++ sync_update_data.sh
@@ -1,7 +1,6 @@
 #/usr/bin/bash
 
-cd ~/covid/
 
-/usr/bin/python3 update_data.py sync_cdc
-/usr/bin/python3 update_data.py sync_hhs
-/usr/bin/python3 update_data.py sync_cdc_wastewater
+/usr/bin/python3 /home/ec2-user/covid/update_data.py sync_cdc
+/usr/bin/python3 /home/ec2-user/covid/update_data.py sync_hhs
+/usr/bin/python3 /home/ec2-user/covid/update_data.py sync_cdc_wastewater
