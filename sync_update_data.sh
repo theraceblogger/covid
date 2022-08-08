1641949bebef1f880ef51d58c6f644c5c2bddea4
Chuck Schultz <theraceblogger@comcast.net>
2022-08-08 16:05:39 -0600

    process tests

--- sync_update_data.sh
+++ sync_update_data.sh
@@ -1,9 +1,13 @@
 #/usr/bin/bash
 
 
+# update raw data
 /usr/bin/python3 /home/ec2-user/covid/update_data.py sync_cases_deaths
 /usr/bin/python3 /home/ec2-user/covid/update_data.py sync_hospitalizations
 /usr/bin/python3 /home/ec2-user/covid/update_data.py sync_wastewater
 /usr/bin/python3 /home/ec2-user/covid/update_data.py sync_tests
 /usr/bin/python3 /home/ec2-user/covid/update_data.py sync_vaccinations
 /usr/bin/python3 /home/ec2-user/covid/update_data.py sync_variants
+
+# process raw data
+/usr/bin/psql -U $db_user_covid -d $db_name_covid -h $db_host_covid -f /home/ec2-user/covid/sql/process_tests.sql
